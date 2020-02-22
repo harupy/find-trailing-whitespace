24f2ca42a6dcf0a8726306b266867210623d8868
harupy <hkawamura0130@gmail.com>
2020-02-22 19:50:38 +0900

    Remvoe debug line and add trailing whitespace

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ tw_lines=""  # Lines containing trailing whitespaces.
 # Iterate through changed files.
 for file in $(git ls-files | sed -e 's/^/.\//')
 do
-  echo $file
   lines=$(egrep -rnIH " +$" $file | cut -f-2 -d ":")
   if [ ! -z "$lines" ]; then
     tw_lines+=$([[ -z "$tw_lines" ]] && echo "$lines" || echo $'\n'"$lines")
@@ -19,7 +18,7 @@ exit_code=0
 # If tw_lines is not empty, change the exit code to 1 to fail the CI.
 if [ ! -z "$tw_lines" ]; then
   echo -e "\n***** Lines containing trailing whitespace *****\n"
-  echo -e "${tw_lines[@]}"
+  echo -e "${tw_lines[@]}" 
   exit_code=1
 fi
 
