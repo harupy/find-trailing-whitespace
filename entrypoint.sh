b4bd062de7872de715035583b9eb4d8b8111fac2
chrisfinazzo <chris.finazzo@gmail.com>
2021-06-04 10:01:30 -0400

    Shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,16 @@ tw_lines=""  # Lines containing trailing whitespaces.
 # TODO (harupy): Check only changed files.
 for file in $(git ls-files | sed -e 's/^/.\//')
 do
-  lines=$(egrep -rnIH " +$" $file | cut -f-2 -d ":")
-  if [ ! -z "$lines" ]; then
-    tw_lines+=$([[ -z "$tw_lines" ]] && echo "$lines" || echo $'\n'"$lines")
+  lines=$(grep -E -rnIH " +$" "$file" | cut -f-2 -d ":")
+  if [ -n "$lines" ]; then
+    tw_lines+="$([[ -z "$tw_lines" ]] && echo "$lines" || echo $'\n'"$lines")"
   fi
 done
 
 exit_code=0
 
 # If tw_lines is not empty, change the exit code to 1 to fail the CI.
-if [ ! -z "$tw_lines" ]; then
+if [ -n "$tw_lines" ]; then
   echo -e "\n***** Lines containing trailing whitespace *****\n"
   echo -e "${tw_lines[@]}"
   echo -e "\nFailed.\n"
